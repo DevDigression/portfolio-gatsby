9481187581b532bffa7d8bc28b66ea7779ff64fc
Chris Rodgers <devdigression@gmail.com>
2019-04-24 20:04:51 -0400

    updated blog header

--- src/components/Sections/Header/index.js
+++ src/components/Sections/Header/index.js
@@ -1,5 +1,19 @@
 import React from 'react'
+import styled from 'styled-components'
 
-const Header = props => <div id={`${props.page}-header`}>{props.page}</div>
+const HeaderText = styled.h1`
+  margin-right: 100px;
+  text-align: center;
+  @media (max-width: 768px) {
+    font-size: 24px;
+    margin: 0;
+  }
+`
+
+const Header = props => (
+  <div id={`${props.page}-header`}>
+    <HeaderText>Dev Digression: A Blog</HeaderText>
+  </div>
+)
 
 export default Header
